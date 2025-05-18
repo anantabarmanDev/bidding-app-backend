f7ee058879a9a24a65855d1e160bae79fb63139c
Ananta Barman <dev.anantabarman@gmail.com>
2025-05-18 16:29:11 +0530

    server again

--- app.js
+++ app.js
@@ -5,11 +5,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
-
+ 
 // server listen
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
@@ -19,3 +15,4 @@ app.listen(PORT, () => {
     console.log("Error", error.message);
   }
 });
+  
